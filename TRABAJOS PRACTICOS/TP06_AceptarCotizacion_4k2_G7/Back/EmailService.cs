78bbed19c06220ddf28e12d6d57f5d7ab13f03a1
Fabrizio Chavez <83493@sistemas.frc.utn.edu.ar>
2024-09-16 14:59:57 -0300

    US Cotizacion Backend con Mail

---
+++ TRABAJOS PRACTICOS/TP06_AceptarCotizacion_4k2_G7/Back/EmailService.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace ISW_TP6_MAIL
+{
+    public class EmailService : IEmailSender, IEmailService
+    {
+        private readonly ILogger _logger;
+        private readonly IConfiguration _configuration;
+
+        public EmailService() { }
+
+        public EmailService(ILogger<EmailService> logger, IConfiguration configuration)
+        {
+            this._logger = logger;
+            this._configuration = configuration;
+        }
+        public async Task SendEmailAsync(EmailDTO emailDTO)
+        {
+            string sendGridApiKey = _configuration["SendGrid:ApiKey"];
+            string emailAddress = "solucionado.mailing.service@gmail.com";
+            string appName = "ISW - Grupo7 - 4K2";
+            var client = new SendGridClient(sendGridApiKey);
+            SendGridMessage msg = await BuildMessage(emailDTO, $"Cotización confirmada - Pedido {GenerateOrderNumber()}", emailAddress, appName);
+
+            var response = await client.SendEmailAsync(msg);
+            if (response.IsSuccessStatusCode)
+            {
+                _logger.LogInformation("Email queued successfully");
+            }
+            else
+            {
+                _logger.LogError("Failed to send email");
+            }
+        }
+
+        public static string GenerateOrderNumber()
+        {
+            const string prefix = "#ORD";
+            int randomNumber = new Random().Next(1000000);
+            string paddedNumber = randomNumber.ToString("D7"); 
+            return $"{prefix}-{paddedNumber}";
+        }
+
+        private async Task<SendGridMessage> BuildMessage(EmailDTO emailDTO, string subject, string emailAddress, string appName)
+        {
+            // Leer el contenido del archivo
+            string htmlContent = File.ReadAllText("email.html");
+            htmlContent = htmlContent.Replace("[DadorCarga]", emailDTO.DadorCarga);
+            htmlContent = htmlContent.Replace("[Calificacion]", emailDTO.Calificacion);
+            htmlContent = htmlContent.Replace("[Transportista]", emailDTO.Transportista);
+            htmlContent = htmlContent.Replace("[FechaRetiro]", emailDTO.FechaRetiro);
+            htmlContent = htmlContent.Replace("[FechaEntrega]", emailDTO.FechaEntrega);
+            htmlContent = htmlContent.Replace("[FormaPago]", emailDTO.FormaPago);
+            htmlContent = htmlContent.Replace("[Importe]", emailDTO.Importe);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress(emailAddress, appName),
+                Subject = subject,
+                PlainTextContent = htmlContent,
+                HtmlContent = htmlContent
+            };
+            msg.AddTo(new EmailAddress(emailDTO.Email));
+            return msg;
+        }
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TRABAJOS PRACTICOS/TP06_AceptarCotizacion_4k2_G7/Back/Program.cs
@@ -0,0 +1,41 @@
+using ISW_TP6_MAIL;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IEmailService, EmailService>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowAllOrigins");
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
